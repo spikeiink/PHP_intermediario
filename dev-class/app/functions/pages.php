86861f4072ddb52a2098b78ed284f311328227a4
Douglas \Maziero <promax.contato@hotmail.com>
2023-03-16 00:35:36 -0300

    Add files via upload
    primeiros cod com função para fazer upload

---
+++ dev-class/app/functions/pages.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+
+function load() {
+    
+
+    $page = filter_input(INPUT_GET,'page',FILTER_UNSAFE_RAW);
+
+
+    
+    $page = (!$page) ? "pages/home.php" : "pages/{$page}.php";
+
+      
+    
+    return $page;
+    
+
+}
+
+    
+
+
+?>
---
+++ dev-class/public/index.php
@@ -0,0 +1,19 @@
+<?php require("./app/functions/pages.php");     ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <div class="container">
+
+    <?php include load()  ?>
+
+    </div>
+</body>
+</html>
